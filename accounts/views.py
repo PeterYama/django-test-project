cdff9f68dd0d66dd7a961d74962e23e02b6ed0a6
= <=>
2020-09-18 22:01:03 +1000

    fixed a bug where data was not showing properly on the template

--- accounts/views.py
+++ accounts/views.py
@@ -3,14 +3,10 @@ from .models import *
 import itertools
 from django.views import generic
 
-class ProfileView(generic.ListView):
-    model = Profile
-    def get_queryset(self):
-        return Profile.objects.filter(id=1)[:5]
-
-    # obj = Image.objects.filter(user__name='Peter Yamamoto')
-    # context = {
-    #     'album' : obj.values()[0]
-    # }
-    # print(obj.values()[0])
-    # return render(request, "accounts/detail.html", context)
+def profile_detail_view(request):
+    obj = Image.objects.filter(user__name='Peter Yamamoto')
+    context = {
+        'album' : obj.values()
+    }
+    print(obj)
+    return render(request, "accounts/profile_detail.html", context)
--- testproj/urls.py
+++ testproj/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view, profile_view
-from accounts.views import ProfileView
+from accounts.views import profile_detail_view
 
 
 urlpatterns = [
     path('', home_view,name='home'),
-    path('detail/', ProfileView.as_view(),name='detail'),
+    path('detail/', profile_detail_view,name='detail'),
     path('profile/', profile_view,name='profile'),
     path('admin/', admin.site.urls),
 ]
